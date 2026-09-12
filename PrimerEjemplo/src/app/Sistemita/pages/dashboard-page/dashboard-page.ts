ab9b03ff43d485206767dc0769a9c07f6cc6906e
Gustavo Méndez <gmendez@probyteg.com>
2026-09-12 14:15:39 -0600

    Funcionalidad de crear y consultar clientes

--- PrimerEjemplo/src/app/Sistemita/pages/dashboard-page/dashboard-page.ts
+++ PrimerEjemplo/src/app/Sistemita/pages/dashboard-page/dashboard-page.ts
@@ -1,10 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './dashboard-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardPage {}
+export class DashboardPage {
+  menuItems = [
+    {
+      route: '/dashboard/crear',
+      title: 'Crear clientes',
+      subtitle: 'Clientes',
+      icon: 'M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z',
+    },
+    {
+      route: '/dashboard/consultar',
+      title: 'Consultar clientes',
+      subtitle: 'Database Manager',
+      icon: 'M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125',
+    },
+  ];
+}
--- PrimerEjemplo/src/app/app.routes.ts
+++ PrimerEjemplo/src/app/app.routes.ts
@@ -7,6 +7,27 @@ export const routes: Routes = [
       import('./Sistemita/pages/dashboard-page/dashboard-page').then(
         (m) => m.DashboardPage,
       ),
+    children: [
+      {
+        path: 'crear',
+        loadComponent: () =>
+          import('./Sistemita/components/crear_cliente/crear_cliente').then(
+            (m) => m.CrearCliente,
+          ),
+      },
+      {
+        path: 'consultar',
+        loadComponent: () =>
+          import('./Sistemita/components/consulta_cliente/consulta_cliente').then(
+            (m) => m.ConsultaCliente,
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
   {
     path: '**',
